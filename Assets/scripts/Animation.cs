23cc272aded5f85d85ea0900c3dc0071290e2684
Aleksey Zakharov <aleksey.zakharov@gmail.com>
2019-01-04 12:36:49 -0800

    Initial Commit

---
+++ Assets/scripts/Animation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour {
+
+    const float EPSI = 0.00001f;
+
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GameObject.Find("T-pose_Player").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!CompareV3xz(Navigation.target, transform.position , EPSI))
+        {
+            if (Navigation.RunCrouch)
+            {
+                animator.SetBool("movement", false);
+                animator.SetBool("crouching", true);
+            }
+            else
+            {
+                animator.SetBool("crouching", false);
+                animator.SetBool("movement", true);
+            }
+        }
+        else
+        {
+            animator.SetBool("crouching", false);
+            animator.SetBool("movement", false);
+        }
+    }
+
+
+    private bool CompareV3xz(Vector3 a, Vector3 b, float epsilon)
+    {
+        float x, z;
+        x = Mathf.Abs(a.x - b.x);
+        if (x > epsilon)
+            return false;
+
+        z = Mathf.Abs(a.z - b.z);
+        return z <= epsilon;
+    }
+    private bool CompareV3xyz(Vector3 a, Vector3 b, float epsilon)
+    {
+        float x, y, z;
+        x = Mathf.Abs(a.x - b.x);
+        if (x > epsilon)
+            return false;
+
+        y = Mathf.Abs(a.y - b.y);
+        if (y > epsilon)
+            return false;
+
+        z = Mathf.Abs(a.z - b.z);
+        return z >= epsilon;
+    }
+}
---
+++ Assets/scripts/Navigation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour {
+
+    public static Vector3 target;
+    public static bool RunCrouch;
+
+    NavMeshAgent agent;
+    public float RunSpeed = 5;
+    public float CrouchSpeed = 3;
+
+    private int interator = 0;
+
+	// Use this for initialization
+	void Start () {
+        RunCrouch = false;
+        agent = GetComponent<NavMeshAgent>();
+        target = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                agent.SetDestination(hit.point);
+                target = hit.point;
+            }
+            RunCrouch = true;
+        }
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            RunCrouch = true;
+            agent.speed = CrouchSpeed;
+        }
+        else
+        {
+            RunCrouch = false;
+            agent.speed = RunSpeed;
+        }
+    }
+
+    
+
+}
